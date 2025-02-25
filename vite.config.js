adc17cce18cc6b65c8e02e90fee2e4a3641c93ad
vittypaul <chamaakuriaravind@gmail.com>
2025-02-25 13:25:20 +0530

    final

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-icons/fa', 'react-icons/fa6'], // Ensure both fa & fa6 are external
-    },
-  },
 });
